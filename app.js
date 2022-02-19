b40ebb92441d5bee2f2928d76cd002fd40f05b74
Karaca7 <karacaylmaz2016@gmail.com>
2022-02-19 15:31:27 +0300

    Create class and  left,right punches etc function
    Methods such as walking forward, walking backward, right punch, left
    punch were added to the class named Ben and related class methods were
    added to the capture functions.

--- app.js
+++ app.js
@@ -14,49 +14,69 @@ const APP = {
       me = document.getElementById("me");
 
       if (e.keyCode == "39") {
-        // console.log("rigth");
-        me = document.getElementById("me");
-        this.position += 10;
-        console.log(this.position);
-        me.style = ` 
-        left:${this.position}px`;
+        this.position = Ben.walkForward(this.position);
       }
 
       if (e.keyCode == "37") {
-        me = document.getElementById("me");
-        this.position -= 10;
-        console.log(this.position);
-        me.style = ` 
-        left:${this.position}px`;
+        this.position = Ben.walkBack(this.position);
       }
 
       if (e.keyCode == "81") {
-        console.log("q");
-        me = document.getElementById("me");
-        me.classList.remove("me");
-
-        setTimeout(() => {
-          me.classList.add("me2");
-        }, 50);
-        me.classList.remove("me2");
-        me.classList.remove("me3");
-        me.classList.add("me");
+        Ben.punchL();
       }
 
       if (e.keyCode == "69") {
-        console.log("e");
-        me = document.getElementById("me");
-        me.classList.remove("me");
-
-        setTimeout(() => {
-          me.classList.add("me3");
-        }, 50);
-        me.classList.remove("me2");
-        me.classList.remove("me3");
-        me.classList.add("me");
+        Ben.punchR();
       }
     },
   },
 };
 
 Vue.createApp(APP).mount("#app");
+
+class Ben {
+  static punchL() {
+    console.log("q");
+    me = document.getElementById("me");
+    me.classList.remove("me");
+
+    setTimeout(() => {
+      me.classList.add("me2");
+    }, 50);
+    me.classList.remove("me2");
+    me.classList.remove("me3");
+    me.classList.add("me");
+  }
+
+  static punchR() {
+    console.log("e");
+    me = document.getElementById("me");
+    me.classList.remove("me");
+
+    setTimeout(() => {
+      me.classList.add("me3");
+    }, 50);
+    me.classList.remove("me2");
+    me.classList.remove("me3");
+    me.classList.add("me");
+  }
+
+  static walkForward(position) {
+    // console.log("rigth");
+    me = document.getElementById("me");
+    position += 10;
+    console.log(this.position);
+    me.style = ` 
+ left:${position}px`;
+    return position;
+  }
+
+  static walkBack(position) {
+    me = document.getElementById("me");
+    position -= 10;
+    console.log(position);
+    me.style = ` 
+    left:${position}px`;
+    return position;
+  }
+}
